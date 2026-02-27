5d152761b086157d6dfd79930de868f6231706f7
Gabriel Ferreira <gsferreiira@gmail.com>
2026-02-27 12:13:49 -0300

    criamos as variaveis de cada elemento e a função

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-/*teste 
+document.getElementById('nome')
+document.getElementById('idade')
+document.getElementById('faculdade')
+document.getElementById('nota')
+document.getElementById('res')
+document.getElementById('btn')
+
+
+
+function mostrar(){
+    var nome = Text(nome.value)
+    var idade = Number(idade.value)
+    var faculdade = Text(faculdade.value)
+    var nota = Number(nota.value)
+}
